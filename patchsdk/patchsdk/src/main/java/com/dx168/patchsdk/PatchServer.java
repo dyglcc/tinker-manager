cb2984a88bdd5d01d0dfc6250f5b800cbc53fd60
dong yuangui <513962116@qq.com>
2017-11-06 09:16:15 +0800

    try to modify https

--- patchsdk/patchsdk/src/main/java/com/dx168/patchsdk/PatchServer.java
+++ patchsdk/patchsdk/src/main/java/com/dx168/patchsdk/PatchServer.java
@@ -15,6 +15,39 @@ import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+// add https
+private static void trustAllHosts()
+        {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager()
+        {
+public java.security.cert.X509Certificate[] getAcceptedIssuers()
+        {
+        return new java.security.cert.X509Certificate[] {};
+        }
+
+public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException
+        {
+        }
+
+public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException
+        {
+        }
+        } };
+
+        // Install the all-trusting trust manager
+        try
+        {
+        SSLContext sc = SSLContext.getInstance("TLS");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e)
+        {
+        e.printStackTrace();
+        }
+        }
+
+
 /**
  * Created by jianjun.lin on 16/7/14.
  */
